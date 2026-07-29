e21f49f4061cd637ea0b76a994728e81c27f4e17
andreahenao0801 <andreahenao0801@gmail.com>
2026-07-29 15:13:32 -0500

    Revert "reto java POO"

--- src/main/java/com/tyba/sqapractice/oop/interfaces/LaserShooter.java
+++
@@ -1,6 +0,0 @@
-package com.tyba.sqapractice.oop.interfaces;
-
-/** TOPIC 3: Interface — defines the laser shooting contract */
-public interface LaserShooter {
-    String fireLaser();
-}
--- src/main/java/com/tyba/sqapractice/oop/interfaces/Swimmable.java
+++
@@ -1,9 +0,0 @@
-package com.tyba.sqapractice.oop.interfaces;
-
-/** TOPIC 3: Interface — defines the swim contract */
-public interface Swimmable {
-    String swim();
-    default String surface() {
-        return "Rising to the surface!";
-    }
-}
